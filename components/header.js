3446bb366908f552bd2ef42609a41a740190cf67
Susmita-Dey <susmitadey475@gmail.com>
2022-10-15 20:12:58 +0530

    fix compile error

--- components/header.js
+++ components/header.js
@@ -3,18 +3,9 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="text-white bg-indigo-900 container mx-auto flex justify-between h-24 items-center">
-      <a href="/">
-        <svg
-          aria-label="My Name logo"
-          xmlns="http://www.w3.org/2000/svg"
-          width="250"
-          height="80"
-        >
-          <text x="120" y="45" fill="white">
-            Susmita Dey
-          </text>
-        </svg>
-      </a>
+      <Link href="/">
+        <p>Susmita Dey</p>
+      </Link>
       <nav>
         <ul className="flex gap-5">
           <li className="hover:text-green-400 focus:underline">
