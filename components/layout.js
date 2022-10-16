6c5d16b6b6ce082965c7dc19da95ba6c7c27ff2a
Susmita-Dey <susmitadey475@gmail.com>
2022-10-16 18:26:19 +0530

    add custom scrollbar

--- components/layout.js
+++ components/layout.js
@@ -1,15 +1,12 @@
-// import { Scrollbar } from "react-scrollbars-custom";
 import Footer from "./footer";
 import Header from "./header";
 
 export default function Layout({ children }) {
   return (
     <>
-      {/* <Scrollbar> */}
-        <Header />
-        <main>{children}</main>
-        <Footer />
-      {/* </Scrollbar> */}
+      <Header />
+      <main>{children}</main>
+      <Footer />
     </>
   );
 }
