ff925f4306fab1f132ff03bb7b40c185cb47c3e1
Susmita-Dey <susmitadey475@gmail.com>
2022-10-17 20:13:42 +0530

    Add bottomToTopButton and 404 page

--- components/layout.js
+++ components/layout.js
@@ -1,12 +1,54 @@
+import { useEffect } from "react";
 import Footer from "./footer";
 import Header from "./header";
 
 export default function Layout({ children }) {
+  useEffect(() => {
+    const showOnPx = 300;
+    const backToTopButton = document.querySelector(".back-to-top");
+
+    const scrollContainer = () => {
+      return document.documentElement || document.body;
+    };
+
+    document.addEventListener("scroll", () => {
+      if (scrollContainer().scrollTop > showOnPx) {
+        backToTopButton.classList.remove("hidden");
+      } else {
+        backToTopButton.classList.add("hidden");
+      }
+    });
+
+    const goToTop = () => {
+      document.body.scrollIntoView({
+        behavior: "smooth",
+      });
+    };
+
+    backToTopButton.addEventListener("click", goToTop);
+  });
+
   return (
     <>
       <Header />
       <main>{children}</main>
       <Footer />
+      <button class="back-to-top hidden">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="back-to-top-icon"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M7 11l5-5m0 0l5 5m-5-5v12"
+          />
+        </svg>
+      </button>
     </>
   );
 }
--- pages/404.js
+++ pages/404.js
@@ -1,4 +1,4 @@
-function Error({ statusCode }) {
+export default function Custom404() {
   return (
     <div className="flex flex-col mx-auto my-12 justify-center items-center">
       <img
@@ -8,11 +8,7 @@ function Error({ statusCode }) {
         width="250rem"
         height="250rem"
       />
-      <h2>
-        {statusCode
-          ? `An error ${statusCode} occurred on server`
-          : "An error occurred on client"}
-      </h2>
+      <h1>404 - Page Not Found</h1>
       <a href="/">
         <button className="bg-blue-900 text-white p-4 my-6 rounded-lg">
           Go back Home
@@ -21,10 +17,3 @@ function Error({ statusCode }) {
     </div>
   );
 }
-
-Error.getInitialProps = ({ res, err }) => {
-  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
-  return { statusCode };
-};
-
-export default Error;
