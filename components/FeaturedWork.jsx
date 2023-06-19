dc5989fe0baaec6c859e2745a28c07ee3825600e
Susmita-Dey <susmitadey475@gmail.com>
2023-06-19 13:49:13 +0530

    fix imports in featured work component

--- components/FeaturedWork.jsx
+++ components/FeaturedWork.jsx
@@ -5,7 +5,7 @@ import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import SectionHeader from "./SectionHeader";
-import Project from "./project";
+import Project from "./Project";
 
 export default function Projects({ work }) {
   useEffect(() => {
